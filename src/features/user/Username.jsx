d29a9856360142c7a6ccc61d3dcaa69bffcdd35d
Jose Copeti <jrcopeti@gmail.com>
2023-12-06 23:25:24 +0100

    tailwind styles

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">José</div>;
+  return <div className="hidden text-sm font-semibold md:block">José</div>;
 }
 
 export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="border-b flex items-center justify-between border-stone-500 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast Pizza Co.
       </Link>
