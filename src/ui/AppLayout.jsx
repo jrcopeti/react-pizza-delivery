9e11fa809c50d8aeca6067c3f75313352746e736
Jose Copeti <jrcopeti@gmail.com>
2023-12-06 19:51:48 +0100

    order form with react router actions

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,7 +5,6 @@ import IsLoading from "./IsLoading";
 
 function AppLayout() {
   const navigation = useNavigation();
-  console.log(navigation);
   const isLoading = navigation.state === "loading";
 
   return (
