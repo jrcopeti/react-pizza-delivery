194864bf9f8a322c14c55259c739385ab4b37eda
Jose Copeti <jrcopeti@gmail.com>
2023-12-11 21:16:33 +0100

    update data without navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
+import Cart from "./features/cart/Cart";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -30,13 +33,14 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
-        action: createOrderAction
+        action: createOrderAction,
       },
       {
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
